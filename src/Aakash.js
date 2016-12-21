cc605664f53ef4a6d9b27e35a782f00a95ed0ded
subexa <subexa@users.noreply.github.com>
2016-12-21 09:14:23 +0545

    error-message

---
+++ src/Aakash.js
@@ -0,0 +1,26 @@
+import React,{ Component } from 'react';
+import './Aakash.css';
+
+class Aakash extends Component {
+	render() {
+		return (
+			<div className="error-container">
+				<div className="error-heading">
+					<div className="error"> Error </div>
+					<div className="error-cross"> ✕ </div>
+				</div>
+				<div className="error-message">
+					dadadajdajdlajdskjdaj jlajdlajda 
+					jaldkjalskdjasj kdjalskjdasjd ajdaueheuhar loerem ajfkajfafa
+					ajfsajflkasj kjfalskjfajf fkjaskjfa sf j.
+				</div>
+				<div className="error-buttons">
+					<button>nope</button>
+					<button>ok</button>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default Aakash;
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import Aakash from './Aakash';
+
+class App extends Component {
+  render() {
+    return (
+        <Aakash />
+    );
+  }
+}
+
+export default App;
